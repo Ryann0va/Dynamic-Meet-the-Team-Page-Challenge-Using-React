1985532de1e21f47c272cfa82bf991e73aa5023a
Ryann0va <rhehn@usf.edu>
2024-07-09 16:58:45 -0400

    Create Gallery.js

---
+++ Gallery.js
@@ -0,0 +1,16 @@
+//U99350821
+
+import React from 'react';
+import Profile from './Profile';
+
+const Gallery = ({ members }) => {
+  return (
+    <div className="gallery">
+      {members.map((member, index) => (
+        <Profile key={index} name={member.name} role={member.role} photo={member.photo} />
+      ))}
+    </div>
+  );
+}
+
+export default Gallery;
