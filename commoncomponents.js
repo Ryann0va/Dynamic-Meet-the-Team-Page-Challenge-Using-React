42bf5ce99e1ca5c167b1d3ddefff995b6963e266
Ryann0va <rhehn@usf.edu>
2024-07-09 17:00:13 -0400

    Create commoncomponents.js

---
+++ commoncomponents.js
@@ -0,0 +1,19 @@
+//U99350821
+
+import React from 'react';
+
+export const Header = ({ title }) => {
+  return (
+    <header>
+      <h1>{title}</h1>
+    </header>
+  );
+}
+
+export const Footer = () => {
+  return (
+    <footer>
+      <p>© 2024 My Company</p>
+    </footer>
+  );
+}
